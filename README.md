2d7241e209dc2f28c747598ee8c9a943b4aa4650
Lyall <me@lyall.lol>
2024-12-15 14:10:54 +0000

    piss

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # A crappy script to check how much lines and characters of code you have written
-
-## Note: requires Node.js to be installed. To leave an argument blank and skip it put `""`
-
-## Usage: `node . "<optional file extensions>" "<optional depth>" "<optional directory path>"`
-
-## Example: `node . ".js .json"`
--- index.js
+++ index.js
@@ -7,22 +7,21 @@ const exclusions = [
 ]
 
 const extensions = process.argv[2]?.split(" ").filter(i => i);
-const depth = process.argv[3] ? Number(process.argv[3]) : null;
+const depth = process.argv[3] ? parseInt(process.argv[3]) : null;
 const dirPath = process.argv[4] || __dirname;
 
 console.log("Finding files");
 const files = getFiles(dirPath);
-// console.log(files);
 console.log(`Reading ${files.length} files`);
 
 let lines = 0;
 let chars = 0;
 
-files.forEach(filePath => {
+for (const filePath of files) {
     const file = fs.readFileSync(filePath, "utf-8");
     lines += file.split("\n").length;
-    chars += file.length;
-});
+    chars += file.replace(/[^\w]/g, "").length;
+};
 
 console.log();
 console.log(`
@@ -34,17 +33,14 @@ Characters: ${chars}
 function getFiles(dirPath, currDepth = 0) {
     const files = [];
 
-    fs.readdirSync(dirPath)
-        .filter(i => exclusions.includes(i) ? false : true)
-        .forEach(fileName => {
-            const joinedPath = path.join(dirPath, fileName);
-            if (fs.lstatSync(joinedPath).isDirectory()) {
-                if ((typeof depth == "number" && currDepth < depth) || typeof depth != "number")
-                    files.push(...getFiles(joinedPath, currDepth + 1));
-            } else {
-                if (extensions?.[0] ? extensions.includes(path.extname(joinedPath)) : true) files.push(joinedPath);
-            }
-        });
+    for (const fileName of  fs.readdirSync(dirPath).filter(i => exclusions.includes(i) ? false : true)) {
+        const fullPath = path.join(dirPath, fileName);
+        if (fs.lstatSync(fullPath).isDirectory()) {
+            if ((depth && currDepth < depth) || depth === null) files.push(...getFiles(fullPath, currDepth + 1));
+        } else {
+            if (extensions?.[0] ? extensions.includes(path.extname(fullPath)) : true) files.push(fullPath);
+        }
+    }
 
     return files;
 }
